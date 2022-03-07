8d0f5f662e66dba7e3091e2eaba1a7491fa38a8d
Solar-Chutney <noam4134@gmail.com>
2022-03-07 18:13:03 +0000

    cross platform audio

--- include/timer.h
+++ include/timer.h
@@ -3,6 +3,36 @@
 
 #include <chrono>
 
+#ifdef _WIN32
+
+class Timer
+{
+public:
+	Timer()
+	{
+		start = std::chrono::high_resolution_clock::now();
+		lastUpdate = start;
+		currentUpdate = start;
+	}
+	void Update()
+	{
+		lastUpdate = currentUpdate;
+		currentUpdate = std::chrono::high_resolution_clock::now();
+	}
+
+	long long FrameElapsed()
+	{
+		return std::chrono::duration_cast<std::chrono::milliseconds>(currentUpdate - lastUpdate).count();
+	}
+
+private:
+	std::chrono::steady_clock::time_point start;
+	std::chrono::steady_clock::time_point lastUpdate;
+	std::chrono::steady_clock::time_point currentUpdate;
+};
+
+#else
+
 class Timer
 {
 public:
@@ -29,6 +59,7 @@ private:
 	std::chrono::time_point<std::chrono::_V2::system_clock, std::chrono::duration<long int, std::ratio<1, 1000000000> > > currentUpdate;
 };
 
+#endif
 
 
 #endif
