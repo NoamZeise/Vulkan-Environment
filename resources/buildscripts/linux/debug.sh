03f102b6eeaa4665ca6553478e606d137573db16
Solar-Chutney <noam4134@gmail.com>
2022-05-19 15:01:31 +0100

    fixed linux build script error

--- resources/buildscripts/linux/debug.sh
+++ resources/buildscripts/linux/debug.sh
@@ -2,10 +2,10 @@ cd build
 cmake .. -G"Ninja Multi-Config" -DCMAKE_EXPORT_COMPILE_COMMANDS=1
 cmake --build . --config Debug
 cd ..
-cp -r resources/audio build/Debug/audio
-cp -r resources/shaders build/Debug/shaders
-cp -r resources/models build/Debug/models
-cp -r resources/textures build/Debug/textures
+cp -r resources/audio build/Debug
+cp -r resources/shaders build/Debug
+cp -r resources/models build/Debug
+cp -r resources/textures build/Debug
 cd build/Debug
 ./Vulkan-Environment
 cd ../..
--- resources/buildscripts/linux/release.sh
+++ resources/buildscripts/linux/release.sh
@@ -2,10 +2,10 @@ cd build
 cmake .. -G"Ninja Multi-Config" -DCMAKE_EXPORT_COMPILE_COMMANDS=1
 cmake --build . --config Release
 cd ..
-cp -r resources/audio build/Release/audio
-cp -r resources/shaders build/Release/shaders
-cp -r resources/models build/Release/models
-cp -r resources/textures build/Release/textures
+cp -r resources/audio build/Release
+cp -r resources/shaders build/Release
+cp -r resources/models build/Release
+cp -r resources/textures build/Release
 cd build/Release
 ./Vulkan-Environment
 cd ../..
