1c9e4ffe7d538a7648dcd1775460efb51edcba70
NoamZeise <noam4134@gmail.com>
2023-01-24 22:58:40 +0000

    vk manager save update

--- src/vulkan_manager.cpp
+++ src/vulkan_manager.cpp
@@ -9,10 +9,18 @@
     if(result_expr != VK_SUCCESS) throw std::runtime_error(error_message);
 
 VulkanManager::VulkanManager(GLFWwindow *window) {
-    throwOnErr(part::create::Instance(&instance), "Failed to create Vulkan Instance");
+    throwOnErr(
+	part::create::Instance(&instance),
+	"Failed to create Vulkan Instance");
 #ifndef NDEBUG
-    throwOnErr(part::create::DebugMessenger(instance, &debugMessenger), "Failed to create Debug Messenger");
+    throwOnErr(
+	 part::create::DebugMessenger(instance, &debugMessenger),
+	 "Failed to create Debug Messenger");
 #endif
-    throwOnErr(glfwCreateWindowSurface(instance, window, nullptr, &windowSurface), "Failed to get Window Surface From GLFW");
+    throwOnErr(
+	 glfwCreateWindowSurface(instance, window, nullptr, &windowSurface),
+	 "Failed to get Window Surface From GLFW");
+
+    
 	
 }
