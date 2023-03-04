e85919f28970a767ed694e0f5b59338ffef69918
NoamZeise <noam4134@gmail.com>
2023-03-04 19:17:08 +0000

    phys device type ranking added

--- src/parts/helper/choose_physical_device.cpp
+++ src/parts/helper/choose_physical_device.cpp
@@ -32,6 +32,10 @@ bool getGraphicsPresentQueueID(VkPhysicalDevice candidateDevice,
     return false;
 }
 
+std::string getPhysicalDeviceTypeStr(VkPhysicalDeviceType type);
+
+bool rankPhysicalDevices(VkPhysicalDeviceProperties bestProps,
+			 VkPhysicalDeviceProperties currentProps);
     
 VkResult choosePhysicalDevice(VkInstance instance,
 			      VkSurfaceKHR surface,
@@ -59,7 +63,9 @@ VkResult choosePhysicalDevice(VkInstance instance,
 	VkPhysicalDeviceProperties deviceProperties;
 	vkGetPhysicalDeviceProperties(gpus.get()[i], &deviceProperties);
 
-	LOG("Candidate Physical Device Name: " << deviceProperties.deviceName); 
+	LOG("Candidate Physical Device Name: " << deviceProperties.deviceName);
+	std::string deviceType = getPhysicalDeviceTypeStr(deviceProperties.deviceType);
+	LOG("Type: " << deviceType);
 	
 	uint32_t queueFamilyCount = 0;
 	vkGetPhysicalDeviceQueueFamilyProperties(gpus.get()[i], &queueFamilyCount, nullptr);
@@ -69,21 +75,64 @@ VkResult choosePhysicalDevice(VkInstance instance,
 						 queueFamilies.get());
 	if (getGraphicsPresentQueueID(gpus.get()[i], surface, queueFamilies.get(), queueFamilyCount,
 				      pGraphicsPresentQueueFamilyId, requestedExtensions)) {
-	    *pPhysicalDevice = gpus.get()[i];
-	    foundSuitable = true;
-	    bestDeviceProperties = deviceProperties;
-	    // prefer discreet GPUs
-	    if(deviceProperties.deviceType == VK_PHYSICAL_DEVICE_TYPE_DISCRETE_GPU)
-		break;
+	    
+	    if(!foundSuitable || rankPhysicalDevices(bestDeviceProperties, deviceProperties)) {
+		*pPhysicalDevice = gpus.get()[i];
+		foundSuitable = true;
+		bestDeviceProperties = deviceProperties;
+	    }
 	}
     }
     if (!foundSuitable)  {
-	std::cerr << "Error: Failed to find device which supports graphics and present queues, "
-	    "as well as the requested extensions\n";
+	LOG_ERROR("Error: Failed to find device which supports graphics and present queues, "
+		  "as well as the requested extensions");
 	return VK_ERROR_FEATURE_NOT_PRESENT;
     }
 
     LOG("Candidate chosen: " << bestDeviceProperties.deviceName);
     
     return result;
+}
+
+
+std::string getPhysicalDeviceTypeStr(VkPhysicalDeviceType type) {
+    if(type == VK_PHYSICAL_DEVICE_TYPE_CPU)
+	return "CPU";
+    if(type == VK_PHYSICAL_DEVICE_TYPE_INTEGRATED_GPU)
+	return "Integrated GPU";
+    if(type == VK_PHYSICAL_DEVICE_TYPE_DISCRETE_GPU)
+	return "Discrete GPU";
+    if(type == VK_PHYSICAL_DEVICE_TYPE_OTHER)
+	return "Other";
+    if(type == VK_PHYSICAL_DEVICE_TYPE_VIRTUAL_GPU)
+	return "Virtual GPU";
+    if(type == VK_PHYSICAL_DEVICE_TYPE_INTEGRATED_GPU)
+	return "IntegrateGPU";
+    return "None";
+}
+
+const size_t DEVICE_RANKING_COUNT = 6;
+const VkPhysicalDeviceType DEVICE_TYPE_RANKINGS[DEVICE_RANKING_COUNT] = {
+    VK_PHYSICAL_DEVICE_TYPE_DISCRETE_GPU,
+    VK_PHYSICAL_DEVICE_TYPE_INTEGRATED_GPU,
+    VK_PHYSICAL_DEVICE_TYPE_CPU,
+    VK_PHYSICAL_DEVICE_TYPE_VIRTUAL_GPU,
+    VK_PHYSICAL_DEVICE_TYPE_OTHER,
+};
+
+// lower is better
+size_t typeRank(VkPhysicalDeviceProperties bestProps) { 
+    for(size_t i = 0; i < DEVICE_RANKING_COUNT; i++) 
+	if(DEVICE_TYPE_RANKINGS[i] == bestProps.deviceType)
+	    return i;
+    return DEVICE_RANKING_COUNT;
+}
+
+bool rankPhysicalDevices(VkPhysicalDeviceProperties bestProps,
+			 VkPhysicalDeviceProperties currentProps) {
+    if(typeRank(currentProps) < typeRank(bestProps)) {
+	return true;
+    }
+    
+    return false;
 }
