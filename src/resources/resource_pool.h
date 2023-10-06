795179f36815b8bdb8253e212c2ed24502956c0c
Noam Zeise <noam4134@gmail.com>
2023-10-06 23:06:11 +0100

    removed unnessecary includes and forward delares in pools

--- src/resources/resource_pool.h
+++ src/resources/resource_pool.h
@@ -1,20 +1,9 @@
 #ifndef VK_ENV_RESOURCE_POOL_H
 #define VK_ENV_RESOURCE_POOL_H
 
-#include <graphics/resources.h>
-#include <graphics/render_config.h>
-#include <volk.h>
-
 #include "texture_loader.h"
 #include "model_render.h"
 #include "font_loader.h"
-
-class DeviceState;
-namespace Resource {
-    class TextureLoader;
-    class ModelRender;
-    class FontLoader;
-};
 
 struct ResourcePool {
     ResourcePool(uint32_t ID, DeviceState base, VkCommandPool pool, RenderConfig config);
