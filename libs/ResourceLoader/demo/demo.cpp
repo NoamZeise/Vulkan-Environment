c98af80bcca0976bb5e4c654e6d6d055d2038422
Noam Zeise <noam4134@gmail.com>
2024-03-02 20:35:39 +0000

    moved outfacing/resource libs to root

--- libs/ResourceLoader/demo/demo.cpp
+++
@@ -1,9 +0,0 @@
-#include <resources/model/model_info.h>
-#include <resources/stb_image.h>
-
-#include <iostream>
-
-int main() {
-    std::cout << "--- Resource Loader Library Demo ---" << std::endl;
-
-}
